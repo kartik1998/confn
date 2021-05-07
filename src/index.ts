6671af2b983b9043bf252888150fc328dd7feb36
Kartik <kartikrawat61@gmail.com>
2021-05-07 12:24:39 +0530

    Add: Env class and it's utility methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(new Date());
+import Env from './confm/env';
+
+const env = new Env();
+
+env.set('live', { set: 'two', ref: 'one' });
+env.override('work', { sde: '1' });
+
+console.log(env);

14bb0b63b8f64029ccbb4c52c3b4bc73e4169806
Kartik <kartikrawat61@gmail.com>
2021-05-07 13:01:04 +0530

    Fix: prettier lint

--- src/confm/env.ts
+++ src/confm/env.ts
@@ -7,21 +7,21 @@ class Env extends Base {
   private hardKeys: string[]; // Array of all the overriden keys. Values of hardKeys CANNOT be updated.
 
   constructor() {
-      super();
-      this.store = {};
-      this.hardKeys = [];
-      this.name = 'env';
+    super();
+    this.store = {};
+    this.hardKeys = [];
+    this.name = 'env';
   }
 
   public set(key: string, value: any): void {
-    if(this.hardKeys.includes(key)) utils.raiseError(`Value of a overriden key ${key} cannot be updated`);
+    if (this.hardKeys.includes(key)) utils.raiseError(`Value of a overriden key ${key} cannot be updated`);
     this.store[key] = value;
   }
   public get(key: string): any {
     return this.store[key];
   }
   public override(key: string, value: any): void {
-    if(this.hardKeys.includes(key)) utils.raiseError(`Value of a overriden key ${key} cannot be overriden`);
+    if (this.hardKeys.includes(key)) utils.raiseError(`Value of a overriden key ${key} cannot be overriden`);
     this.hardKeys.push(key);
     this.store[key] = value;
   }

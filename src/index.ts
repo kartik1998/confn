e5dc286f9bae6da72a9ca971dcb8d541d6106f15
Kartik <kartikrawat61@gmail.com>
2021-05-07 13:42:56 +0530

    Add: method to hardSet a key

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,6 @@ env.set('live', { set: 'two', ref: 'one' });
 env.override('work', { sde: '1' });
 
 console.log(env);
+console.log(env.get('npm_config_save_dev'));
+console.log(env.hardSet('npm_config_save_dev', 'yoo'));
+console.log(env.get('npm_config_save_dev'));
